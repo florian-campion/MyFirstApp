15b93c45345a4439da62f4455a47e1568b051055
florian-campion <90897804+florian-campion@users.noreply.github.com>
2024-01-29 15:04:09 +0100

    domain package

--- app/src/main/java/fr/solacroup/myproject/MainActivity.java
+++ app/src/main/java/fr/solacroup/myproject/MainActivity.java
@@ -1,14 +1,26 @@
 package fr.solacroup.myproject;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView welcomeMessageTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        welcomeMessageTextView = findViewById(R.id.welcome_message);
+        welcomeMessageTextView.setText(getResources().getText(R.string.hello_world));
+
     }
+
 }
