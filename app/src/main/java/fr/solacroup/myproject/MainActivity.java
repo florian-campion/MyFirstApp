783682861fed4fb0bf72ac87440566a1e222dfb1
florian-campion <90897804+florian-campion@users.noreply.github.com>
2024-01-30 09:39:04 +0100

    domain package

--- app/src/main/java/fr/solacroup/myproject/MainActivity.java
+++ app/src/main/java/fr/solacroup/myproject/MainActivity.java
@@ -18,9 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        welcomeMessageTextView = findViewById(R.id.welcome_message);
-        welcomeMessageTextView.setText(getResources().getText(R.string.hello_world));
-
+        this.welcomeMessageTextView = findViewById(R.id.welcome_message);
+        this.welcomeMessageTextView.setText(getResources().getText(R.string.hello_world) + "Solacroup"
+        );
     }
-
 }
