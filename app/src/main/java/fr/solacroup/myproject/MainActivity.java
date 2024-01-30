5087bb6362edc7761e42145f05fd10d48b17d49f
florian-campion <90897804+florian-campion@users.noreply.github.com>
2024-01-30 11:15:41 +0100

    view package

--- app/src/main/java/fr/solacroup/myproject/MainActivity.java
+++ app/src/main/java/fr/solacroup/myproject/MainActivity.java
@@ -1,25 +1,14 @@
 package fr.solacroup.myproject;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.view.View;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
-
-    private TextView welcomeMessageTextView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.welcomeMessageTextView = findViewById(R.id.welcome_message);
-        this.welcomeMessageTextView.setText(getResources().getText(R.string.hello_world) + "Solacroup"
-        );
     }
 }
---
+++ app/src/main/java/fr/solacroup/myproject/view/TasksFragment.java
@@ -0,0 +1,35 @@
+package fr.solacroup.myproject.view;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fr.solacroup.myproject.R;
+
+public class TasksFragment extends Fragment {
+
+    public TasksFragment() {
+        // Required empty public constructor
+    }
+
+    public static TasksFragment newInstance(String param1, String param2) {
+        TasksFragment fragment = new TasksFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tasks, container, false);
+    }
+}
