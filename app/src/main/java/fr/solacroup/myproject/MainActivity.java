3ac89012ffef718575437db89166152675fa3d72
florian-campion <90897804+florian-campion@users.noreply.github.com>
2024-01-31 09:27:39 +0100

    view package

--- app/src/main/java/fr/solacroup/myproject/MainActivity.java
+++ app/src/main/java/fr/solacroup/myproject/MainActivity.java
@@ -5,7 +5,20 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> tasks = new ArrayList<>();
+
+    public ArrayList<String> getTasks() {
+        return tasks;
+    }
+
+    public void addTask(String task) {
+        this.tasks.add(task);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/fr/solacroup/myproject/view/TasksFragment.java
+++ app/src/main/java/fr/solacroup/myproject/view/TasksFragment.java
@@ -1,13 +1,26 @@
 package fr.solacroup.myproject.view;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import fr.solacroup.myproject.MainActivity;
 import fr.solacroup.myproject.R;
 
 public class TasksFragment extends Fragment {
@@ -16,7 +29,7 @@ public class TasksFragment extends Fragment {
         // Required empty public constructor
     }
 
-    public static TasksFragment newInstance(String param1, String param2) {
+    public static TasksFragment newInstance() {
         TasksFragment fragment = new TasksFragment();
         return fragment;
     }
@@ -30,6 +43,47 @@ public class TasksFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tasks, container, false);
+        return inflater.inflate(
+                R.layout.fragment_tasks, container, false
+        );
+    }
+
+    private ListView listView;
+    private ArrayAdapter<String> tasksAdapter;
+    private FloatingActionButton floatingActionButton;
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Init variables
+        listView = view.findViewById(R.id.tasks_list_view);
+        floatingActionButton = view.findViewById(R.id.add_task_button);
+        // Alter
+        ((MainActivity) requireActivity()).addTask("Cours Android");
+        floatingActionButton.setOnClickListener(view1 -> openDialogAddTask());
+        tasksAdapter = new ArrayAdapter<>(
+                requireActivity(),
+                R.layout.task_item,
+                R.id.task_title_text_view,
+                ((MainActivity) requireActivity()).getTasks()
+        );
+        listView.setAdapter(tasksAdapter);
+    }
+
+    private void openDialogAddTask() {
+        EditText editText = new EditText(getContext());
+        AlertDialog dialog = new AlertDialog
+                .Builder(requireActivity())
+                .setTitle("Ajouter une tâche")
+                .setMessage("Que souhaitez-vous faire ?")
+                .setView(editText)
+                .setPositiveButton("Ajouter",new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        tasksAdapter.add(editText.getText().toString());
+                    }
+                })
+                .setNegativeButton("Annuler", null)
+                .create();
+        dialog.show();
     }
 }
